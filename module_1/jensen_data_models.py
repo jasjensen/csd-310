16e3eae3c52641cffeac03c047f6ee7c4d1d0004
jasjensen <104651412+jasjensen@users.noreply.github.com>
2023-01-10 21:43:25 -0600

    Create jensen_data_models.py

---
+++ module_1/jensen_data_models.py
@@ -0,0 +1,31 @@
+{
+    "first_name": "Bruce",
+    "last_name": "Wayne",
+    "Roles": [
+ {
+            "type": "Household",
+            "chore": "Clean Dishes"
+        },
+        {
+            "type": "Work",
+            "chore": "Do Assignment."
+        }
+    ]
+"Birthdate": [
+        {
+"day": "30",
+              "month": "January"
+"year": "2001"
+        }
+    ]
+    "Dependents": [
+        {
+            "first_name": "Jack",
+            "last_name": "Sparrow"
+        },
+        {
+            "first_name": "Harry",
+            "last_name": "Potter"
+        }
+    ]
+}
