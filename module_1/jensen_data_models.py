ca4f2e1d09ab165c0edff6a28c6635c292267a62
jasjensen <104651412+jasjensen@users.noreply.github.com>
2023-01-10 21:47:04 -0600

    Update jensen_data_models.py

--- module_1/jensen_data_models.py
+++ module_1/jensen_data_models.py
@@ -1,31 +1,31 @@
 {
-    "first_name": "Bruce",
-    "last_name": "Wayne",
+    "first_name": 'Bruce',
+    "last_name": 'Wayne',
     "Roles": [
  {
-            "type": "Household",
-            "chore": "Clean Dishes"
+            "type": 'Household',
+            "chore": 'Clean Dishes'
         },
         {
-            "type": "Work",
-            "chore": "Do Assignment."
+            "type": 'Work',
+            "chore": 'Do Assignment',
         }
     ]
 "Birthdate": [
         {
-            "day": "30",
-            "month": "January",
-            "year": "2001"
+            "day": 30,
+            "month": 'January',
+            "year": '2001'
         }
     ]
     "Dependents": [
         {
-            "first_name": "Jack",
-            "last_name": "Sparrow"
+            "first_name": 'Jack',
+            "last_name": 'Sparrow'
         },
         {
-            "first_name": "Harry",
-            "last_name": "Potter"
+            "first_name": 'Harry',
+            "last_name": 'Potter'
         }
     ]
 }
