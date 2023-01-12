1a53d811a7dbcc25728e8065674f4fa186fdd263
jasjensen <104651412+jasjensen@users.noreply.github.com>
2023-01-11 23:16:57 -0600

    Create jensen_pytech.py

---
+++ module_2/jensen_pytech.py
@@ -0,0 +1,95 @@
+"NoSQL Data Structure"
+{
+    {
+    "student": 
+    
+        "student_id": "78925",
+        "first_name": "Raplh",
+        "last_name": "Reader"
+    }
+    }
+    
+    "enrollments": {
+        [
+        "term": "fall2023",
+        "gpa": "3.8",
+        "start_date": [
+            "month": "jan",
+            "day": "02",
+            "year": "2023"
+        ]
+
+        ]
+        "end_date": [,
+            "month": "mar",
+            "day": "27",
+            "year": "2023" 
+        ]
+        }
+        "course": [
+            "course_id": "410",
+            "description": "cybr",
+            "instructor": "Prof._Le",
+            "grade": "95%"
+        ]
+        [
+            "course_id": "420",
+            "decription": "cybr",
+            "instructor": "Prof._Geron",
+            "grade": "91%"
+        ]
+        
+        
+         
+          {
+        "term": "spr2024",
+        "gpa": "3.5",
+        "start_date": [
+            "month": "apr",
+            "day": "12",
+            "year": "2024"
+        ]
+        "end_date":[
+            "month": "jul",
+            "day": "16",
+            "year": "2024"
+        ]
+        "course": [
+            "course_id": "236",
+            "decription": "cybersecurity",
+            "instructor": "Prof._James",
+            "grade": "98%"
+        ]
+        
+    }
+    
+
+    {
+        "course": [
+            "course_id": "336",
+            "decription": "pen._testing",
+            "instructor": "Prof._Wan"
+            "grade": "92%"
+        ],
+        [
+            "course_id": "389",
+            "decription": "application",
+            "instructor": "Prof._Davis"
+            "grade": "96%"
+        ],
+        "course": [
+            "course_id": "498",
+            "decription": "python",
+            "instructor": "Prof._Williams"
+            "grade": "92%"
+        ],
+        [
+            "course_id": "521",
+            "decription": "java",
+            "instructor": "Prof._Davidson"
+            "grade": "98%"
+        ]
+    }   
+    
+
+}
