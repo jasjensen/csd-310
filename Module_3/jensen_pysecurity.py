03f3d9b758565a1c496cbcc56db7b19a2d236f61
jasjensen <104651412+jasjensen@users.noreply.github.com>
2023-01-20 09:39:16 -0600

    Create jensen_pysecurity.py

---
+++ Module_3/jensen_pysecurity.py
@@ -0,0 +1,41 @@
+Publisher, Book and Book_Author 
+{
+    "Publisher":[{
+    publisher_code: 1356,
+    publisher_name : "John Smith",
+    publisher_address: "22596 Lincoln Ave. New York, NY 68953",
+   
+   "Book":[{
+
+     book_ISBN : 49654986,
+     book_name: "Beginning Java",
+     book_price: 95.3,
+      
+    "Book_Author": [{
+         book_ISBN: 6876543,
+         author,email: "WilliamWalls@greatbooks.com"
+        
+        
+         }]
+       
+       
+     }] 
+   
+
+   }]
+
+}
+
+
+Author
+{
+   "Author":[{
+   author_email: "WilliamWalls@greatbooks.com",
+   author_first_name: "William",
+   author_last_name: "Walls",
+   author_phone : 2083681952
+       
+       
+       ]}
+
+}
