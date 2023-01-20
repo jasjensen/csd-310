1976492c0a918f0db7ee6f5791809f1d0e16aef3
jasjensen <104651412+jasjensen@users.noreply.github.com>
2023-01-20 09:24:31 -0600

    Create PySecurity.py

---
+++ module_2/PySecurity.py
@@ -0,0 +1,33 @@
+Publisher, Book and Book_Author 
+{
+    publisher_code: 1356,
+    publisher_name : "John Smith",
+    publisher_address: "The House on the Hill",
+   
+   "Book":[{
+
+     book_ISBN : 49654986,
+     book_name: "Beginning Java",
+     book_price: 95.3,
+      
+    "Book_Author": [{
+         book_ISBN: 6876543,
+         author,email: "WilliamWalls@greatbooks.com"
+
+     }] 
+   
+
+   }]
+
+}
+
+
+Author
+{
+   author_email: "WilliamWalls@greatbooks.com",
+   author_first_name: "William",
+   author_last_name: "Walls",
+   author_phone : 2083681952
+  
+
+}
