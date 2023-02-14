d72ca2f044728b1ab9d6870c9d7965255d95fb30
jasjensen <104651412+jasjensen@users.noreply.github.com>
2023-02-14 14:47:50 -0600

    Update pysports_update_and_delete.py

--- module_9/pysports_update_and_delete.py
+++ module_9/pysports_update_and_delete.py
@@ -2,18 +2,19 @@ import mysql.connector
 from mysql.connector import errorcode
 
 
-""" database config object """
 config = {
     "user": "pysports_user",
     "password": "MySQL8IsGreat!",
-    "host": "localhost",
+    "host": "127.0.0.1",
     "database": "pysports",
     "raise_on_warnings": True
 }
 
 
 def show_players(cursor, title):
- 
+    """ method to execute an inner join on the player and team table, 
+        iterate over the dataset and output the results to the terminal window.
+    """
 
     cursor.execute("SELECT player_id, first_name, last_name, team_name FROM player INNER JOIN team ON player.team_id = team.team_id")
 
@@ -25,14 +26,13 @@ def show_players(cursor, title):
         print("  Player ID: {}\n  First Name: {}\n  Last Name: {}\n  Team Name: {}\n".format(player[0], player[1], player[2], player[3]))
 
 try:
-
-    db = mysql.connector.connect(**config)
+    db = mysql.connector.connect(**config) 
 
     cursor = db.cursor()
 
     add_player = ("INSERT INTO player(first_name, last_name, team_id)"
                  "VALUES(%s, %s, %s)")
-    
+
     player_data = ("Smeagol", "Shire Folk", 1)
 
     cursor.execute(add_player, player_data)
@@ -47,15 +47,20 @@ try:
 
     show_players(cursor, "DISPLAYING PLAYERS AFTER UPDATE")
 
-    delete_player = ("DELETE FROM player WHERE first_name = 'Gollum'")
+    delete_player = ("DELETE FROM player WHERE first_name = 'Smeagol'")
 
     cursor.execute(delete_player)
+
+    insert_player = ("INSERT player SET first_name = 'Smeagol', last_name = 'Shire Folk', team_id = '1'")
+
+    cursor.execute(insert_player)
 
     show_players(cursor, "DISPLAYING PLAYERS AFTER DELETE")
 
     input("\n\n  Press any key to continue... ")
 
 except mysql.connector.Error as err:
+    
 
     if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
         print("  The supplied username or password are invalid")
